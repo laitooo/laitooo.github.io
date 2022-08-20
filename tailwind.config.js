d585a724c576c3020fca881b7ea4c45901e6b0cf
Laitooo <killuasan25@gmail.com>
2022-08-20 21:05:06 +0200

    added flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("daisyui")
+    require("daisyui"),
+    require("flowbite/plugin")
   ],
   daisyui: {
     styled: true,
